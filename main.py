42df926de9e097d44a6f3970c8c148d5e8abc004
FlipGoncalves <filipeg@ua.pt>
2022-10-12 09:59:34 +0100

    main branch

--- main.py
+++ main.py
@@ -1,129 +1,157 @@
 #!/usr/bin/env python
-# --------------------------------------------------
-# Typing Test
+
+# ---------- Typing Test ------------
 # Filipe Goncalves, 98083
-# Hugo Hahimoto, 92226
 # Guilherme Cajeira, 93138
+# Hugo Hahimoto, 92226
+
 # PSR, October 2022.
-# --------------------------------------------------
+# -----------------------------------
 
-import readchar
-from pprint import pprint
 import argparse
-from colorama import Fore, Style
-from collections import namedtuple
+import readchar
 import random
-import time
+from colorama import Fore, Back, Style
+from collections import namedtuple
+from time import time, ctime
+from pprint import pprint
 
-Input = namedtuple('KeyInput', ['requested', 'received', 'duration'])
+
+Input = namedtuple('Input', ['requested', 'received', 'duration'])
 
 INPUTS = []
 
-def playGame(args):
+# Function responsible for calculating statistics
+#   Return: Dictionary with game statistics
+def Statistics(hits, total, start_game, end_game):
+    accuracy = hits / total
+    game_duration = end_game - start_game
+    average_duration = (sum([dur.duration for dur in INPUTS]) / len(INPUTS)) if len(INPUTS) > 0 else 0
+    hit_average_duration = (sum([dur.duration for dur in INPUTS if dur.requested == dur.received]) / hits) if hits > 0 else 0
+    miss_average_duration = (sum([dur.duration for dur in INPUTS if dur.requested != dur.received]) / (total-hits)) if (total-hits) > 0 else 0
 
-    # start the game
-    game_start = time.time()
-    count = 0
-    right_hits = 0
-
-    # loop
-    while True:
-
-        # character between a (97 in ASCII) and z (122 in ASCII)
-        character = chr(random.randint(97, 122))
-        print("Type letter: " + Fore.CYAN + character + Style.RESET_ALL)
-        
-        # get received key
-        time_start = time.time()
-        k = readchar.readkey()
-        time_end = time.time()
-        count += 1
-
-        INPUTS.append(Input(character, k, time_end-time_start))
-
-        end_game = time.time()
-
-        # if key is space
-        if k == ' ':
-            print("\nUser pressed SPACE key - END GAME")
-            break
-
-        print("You typed letter: " + (Fore.GREEN if k == character else Fore.RED) + k + Style.RESET_ALL)
-        
-        # right key
-        if k == character:
-            right_hits += 1
-
-        # break the loop depending on time mode
-        if args.use_time_mode == True:
-            if end_game - game_start >= args.max_value:
-                print("\nCurrent test duration " + Fore.LIGHTMAGENTA_EX + str(end_game - game_start) + Style.RESET_ALL + " exceeds maximum of " + Fore.CYAN + str(args.max_value) + Style.RESET_ALL) 
-                break
-        else:
-            if count >= args.max_value:
-                print("\nCurrent test inputs " + Fore.LIGHTGREEN_EX + str(count) + Style.RESET_ALL + " reaches maximum of " + Fore.LIGHTBLUE_EX + str(args.max_value) + Style.RESET_ALL) 
-                break
-
-    # return values
-    return right_hits, count, end_game, game_start
-    
-
-def main():
-
-    # Define argparse inputs
-    parser = argparse.ArgumentParser(description='Definition of test mode')
-    parser.add_argument('-utm','--use_time_mode', action='store_true', help='Max number of secs for time mode or maximum number of inputs for number of inputs mode.', required=False)
-    parser.add_argument('-mv','--max_value', type=int, help='Max number of seconds for time mode or maximum number of inputs for number of inputs mode.', required=False)
-
-    # Parse arguments
-    args = parser.parse_args()
-
-    # check if arguments are correct
-    if args.max_value == None or args.max_value <= 0:
-        print("Inputted Arguments are Invalid!")
-        exit(0)
-
-
-    # print args
-    
-
-    # print type of the game
-    if args.use_time_mode == False:
-        print(f"Test running up to {args.max_value} inputs.")
-    else:
-        print(f"Test running up to {args.max_value} seconds.")
-
-    # start the game
-    print("To start the game press any key: ")
-    start_key = readchar.readchar()
-
-    # play the game
-    right_hits, count, end_game, game_start = playGame(args)
-
-
-    print(Fore.YELLOW + "Test Finished!" + Style.RESET_ALL)
-
-    # type miss average duration
-    miss_dur = sum([dur.duration for dur in INPUTS if dur.requested != dur.received])
-    miss = count-right_hits
-
-    # statistics
     statistics = {
-        'accuracy': right_hits / count,
+        'accuracy': accuracy,
         'inputs': INPUTS,
-        'number_of_hits': right_hits,
-        'number_of_types': count,
-        'test_duration': end_game-game_start,
-        'test_end': time.ctime(end_game),
-        'test_start': time.ctime(game_start),
-        'type_average_duration': (sum([dur.duration for dur in INPUTS]) / len(INPUTS)) if len(INPUTS) > 0 else 0,
-        'type_hit_average_duration': (sum([dur.duration for dur in INPUTS if dur.requested == dur.received]) / right_hits) if right_hits > 0 else 0,
-        'type_miss_average_duration': (miss_dur / miss) if miss > 0 else 0
+        'number_of_hits': hits,
+        'number_of_types': total,
+        'test_duration': game_duration,
+        'test_end': ctime(end_game),
+        'test_start': ctime(start_game),
+        'type_average_duration': average_duration,
+        'type_hit_average_duration': hit_average_duration,
+        'type_miss_average_duration': miss_average_duration
     }
 
+    return statistics
 
-    print()
-    pprint(statistics)
+# Function responsible for realization the game
+#   Return: Values needed to calculate statistics
+def TypingTest(args):
+    # time the start of the game
+    start_game = time()
 
-if __name__ == '__main__':
+    # number of types and number of successful types
+    total = 0
+    hits = 0
+
+    while True:
+        # get random char from 'a' (ASCII code is 97) to 'z' (ASCII code is 122)
+        char_random = chr(random.randint(97, 122))
+        print("Type letter: " + Style.BRIGHT + Fore.BLUE + char_random + Style.RESET_ALL)
+
+        # time the game
+        time_start_typed = time()
+        # read character from user
+        char_typed = readchar.readkey()
+        # time the game
+        time_end_typed = time()
+
+        # calculate time spending since first shown the letter
+        char_typed_duration = time_end_typed - time_start_typed
+        # total typed keys
+        total += 1
+
+        # pressed space key
+        if char_typed == ' ':
+            print("\nUser pressed " + Style.BRIGHT + Fore.RED + "SPACE" + Style.RESET_ALL + " key - " + Back.RED + Style.BRIGHT + Fore.WHITE + "END GAME" + Style.RESET_ALL)
+            break
+        
+        # append the object to the INPUTS list
+        INPUTS.append(Input(char_random, char_typed, char_typed_duration))
+
+        # number of successful inputs
+        if char_typed == char_random:
+            hits += 1
+
+        print("You typed letter: " + (Style.BRIGHT + Fore.GREEN if char_typed == char_random else Style.BRIGHT + Fore.RED) + char_typed + Style.RESET_ALL)
+
+        # time the possible end of the game
+        end_game = time()
+        # with time mode True
+        if args.use_time_mode == True:
+            # check if there is time available
+            if end_game - start_game >= args.max_value:
+                print("\nCurrent test duration " + Style.BRIGHT + Fore.LIGHTGREEN_EX + str(end_game - start_game) + Style.RESET_ALL + " exceeds maximum of " + Style.BRIGHT + Fore.LIGHTBLUE_EX + str(args.max_value) + Style.RESET_ALL) 
+                break
+        else:
+            # check if there are inputs available
+            if total >= args.max_value:
+                print("\nCurrent test inputs " + Style.BRIGHT + Fore.LIGHTGREEN_EX + str(total) + Style.RESET_ALL + " reaches maximum of " + Style.BRIGHT + Fore.LIGHTBLUE_EX + str(args.max_value) + Style.RESET_ALL) 
+                break
+    
+    return Statistics(hits, total, start_game, end_game)
+
+
+def main():
+    # Define argparse inputs:
+    #   - use_time_value: TRUE, if the user wants to define the maximum number of seconds
+    #                     FALSE, if the user wants to define the maximum number of inputs
+    #   - max_value: define maximum number of seconds if use_time_value is TRUE
+    #                define maximum number of inputs if use_time_value is FALSE
+    parser = argparse.ArgumentParser(description='Definition of ' + Style.BRIGHT + Fore.BLUE + 'test ' +  Style.RESET_ALL + 'mode')
+    parser.add_argument('-utm', '--use_time_mode', action='store_true', help='Max number of secs ' + Style.BRIGHT + Fore.RED + 'for time ' +  Style.RESET_ALL + 'mode or maximum number of inputs ' + Style.BRIGHT + Fore.RED + 'for ' +  Style.RESET_ALL + 'number of inputs mode.', required=False)
+    parser.add_argument('-mv', '--max_value', type=int, help='Max number of seconds ' + Style.BRIGHT + Fore.RED + 'for time ' +  Style.RESET_ALL + 'mode or maximum number of inputs ' + Style.BRIGHT + Fore.RED + 'for ' +  Style.RESET_ALL + 'number of inputs mode.', required=False)
+    args = parser.parse_args()
+
+    # Print args for the user to see the chosen arguments
+    print(args)
+
+    # If the arguments are invalid, the test does not start, showing an error message
+    if args.max_value == None or args.max_value <= 0:
+        print(Back.RED + Style.BRIGHT + Fore.WHITE + "Inputted arguments are invalid!" + Style.RESET_ALL )
+        exit(0)
+
+    # Print the game (Typing Test) and the group membres
+    print('---------------------------------')
+    print('|        ' + Style.BRIGHT + Fore.RED + 'PSR ' + Style.RESET_ALL + 'Typing Test' + '        |')
+    print('|                               |')
+    print('|            ' + Style.BRIGHT + Fore.BLUE + 'Grupo 3' + Style.RESET_ALL + '            |')
+    print('|        Filipe Goncalves       |')
+    print('|       Guilherme Cajeira       |')
+    print('|         Hugo Hashimoto        |')
+    print('---------------------------------\n')
+
+    # Print the type of game the user decided to play
+    if args.use_time_mode == False:
+        print("Test running up up " + str(args.max_value) + " inputs.")
+    else:
+        print("Test running up up " + str(args.max_value) + " seconds.")
+
+    # Print the condition to start playing (press any key)
+    print("Press " + Back.CYAN + Style.BRIGHT + Fore.WHITE + "any key" + Style.RESET_ALL + " to start the test")
+    start_key = readchar.readchar()
+
+    # Call the function responsible for realization the game
+    my_dict = TypingTest(args)
+
+    # Print the game is finish
+    print(Style.BRIGHT + Fore.YELLOW + "Test Finished!\n" + Style.RESET_ALL)
+
+    # Print game statistics
+    pprint(my_dict)
+
+
+
+if __name__ == "__main__":
     main()
